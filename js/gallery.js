55be4b2fbd39607754269f68d4fede6afb6a3707
Anton L. <blizzbox88@gmail.com>
2024-11-23 16:16:10 +0200

    initials

---
+++ js/gallery.js
@@ -0,0 +1,141 @@
+"use strict";
+
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galleryContainer = document.querySelector(".gallery");
+
+const gallerySample = images.map(
+    ({ preview, original, description }) => `
+  <li class="gallery-item">
+    <a class="gallery-link" href="${original}">
+      <img
+        class="gallery-image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+`
+  )
+  .join("");
+
+galleryContainer.innerHTML = gallerySample;
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  if (event.target.nodeName === 'IMG') {
+    const imageUrl = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+      <div class="modal">
+        <div class="modal-content">
+          <img src="${imageUrl}" width="1112" height="auto">
+          <div class="modal-text">${event.target.alt}</div>
+        </div>
+      </div>
+    `);
+
+    instance.show();
+    
+    const modal = document.querySelector('.modal');
+    const modalContent = modal.querySelector('.modal-content');
+    const modalText = modal.querySelector('.modal-text');
+
+   setTimeout(() => {
+      modal.classList.add('show');
+    }, 50)
+
+    modalText.style.width = modalContent.clientWidth + 'px';
+
+    setTimeout(() => {
+      modalText.classList.add('show-text');
+    }, 500);
+
+
+    document.body.style.overflow = 'hidden';
+
+
+    modal.addEventListener('click', (event) => {
+      if (event.target !== event.currentTarget) {
+        return;
+      }
+
+      modalText.classList.remove('show-text');
+      modal.classList.remove('show');
+
+      setTimeout(() => {
+        instance.close();
+        document.body.style.overflow = 'auto';
+      }, 500);
+  
+    });
+  }
+});
